d993d1966cb7f204d1cf2f2ed63048d14cd55d1b
kimyeonghwi <pwrkim12@gmail.com>
2021-08-10 16:22:48 +0900

    자바스크립트

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ import getType from './getType.js'
 import getRandom from './getRandom.js'
 
 
-
-function asb() {
-  
-
-if (true){
-  const a = 1 
-  
-}
- console.log(a)
+function user(first , last ){
+  this.firstname = first
+  this.lastname = last
 }
 
-asb()
+user.prototype.getFullName = function () {
+  return `${this.firstname} ${this.lastname}`
+}
+
+
+//생성자 함수 하나의 객체 데이터가 생성된다.
+const heropy = new user ('heropy' , 'park')
+const am = new user ('am' , 'am')
+const sd = new user ('sd' , 'sd')
+
+console.log(heropy.getFullName())
+console.log(am.getFullName())
+console.log(sd.getFullName())
